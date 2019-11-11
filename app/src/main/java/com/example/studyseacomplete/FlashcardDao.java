9c41001eee8d9cb6a1e7bf7a93dca6753a3bc11d
anushkadewoolkar <dewoolkar6949@students.d211.org>
2019-11-10 21:50:53 -0600

    we're officially done

--- app/src/main/java/com/example/studyseacomplete/FlashcardDao.java
+++ app/src/main/java/com/example/studyseacomplete/FlashcardDao.java
@@ -11,17 +11,25 @@ import androidx.room.Update;
 
 import java.util.List;
 
-@Dao
+@Dao //DAO (Data Access Object) defines the methods that access the database
+//https://developer.android.com/reference/android/arch/persistence/room/Dao
+//https://medium.com/androiddevelopers/7-pro-tips-for-room-fbadea4bfbd1
+//https://medium.com/@ajaysaini.official/building-database-with-room-persistence-library-ecf7d0b8f3e9
 public interface FlashcardDao {
-    @Query("SELECT * FROM flashcard")
+    //Query requests data
+    @Query("SELECT * FROM flashcard") //https://developer.android.com/reference/android/arch/persistence/room/Query.html
+    //gets all flashcards
     List<Flashcard> getAll();
 
     @Insert
-    void insertAll(Flashcard... flashcards);
+        //inserts all flashcards into database
+    void insertAll(Flashcard... flashcards); //https://developer.android.com/reference/android/arch/persistence/room/Insert
 
     @Delete
-    void delete(Flashcard flashcard);
+        //deletes flashcard from database
+    void delete(Flashcard flashcard); //https://developer.android.com/reference/android/arch/persistence/room/Delete
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
+    @Update(onConflict = OnConflictStrategy.REPLACE) //https://developer.android.com/reference/android/arch/persistence/room/Update
+        //updates flashcard info in database
     void update(Flashcard flashcard);
 }
